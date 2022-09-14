2fe728208778db68085a4412e36cef947a7eb32d
Mathias Arlaud <mathias.arlaud@gmail.com>
2022-09-14 09:37:35 +0200

    exercice 4 - solution

--- src/Subscription/Entity/Subscription.php
+++ src/Subscription/Entity/Subscription.php
@@ -4,14 +4,25 @@ declare(strict_types=1);
 
 namespace App\Subscription\Entity;
 
+use ApiPlatform\Metadata\ApiProperty;
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Post;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Uid\Uuid;
 use Symfony\Component\Validator\Constraints as Assert;
 
+#[ApiResource(
+    operations: [
+        new Post(),
+        new Delete(),
+    ],
+)]
 #[ORM\Entity]
 class Subscription
 {
     public function __construct(
+        #[ApiProperty(readable: false, writable: false)]
         #[ORM\Id]
         #[ORM\Column(type: 'uuid', unique: true)]
         #[ORM\GeneratedValue(strategy: 'CUSTOM')]
